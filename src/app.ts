21216b1bb69a6e6f257ca4bfbd72f25afd694574
Joao Paulo Stival <joaopaulosstival@gmail.com>
2023-06-14 10:01:18 -0300

    4

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,6 @@ const app = express()
 app.use(express.json())
 
 app.use("/", route)
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     ok: true,
-//     msg: "hi from my method",
-//   })
-// })
 
 app.listen(3333, () => "server running on port 3333")
 
